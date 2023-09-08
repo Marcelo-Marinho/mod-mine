d393672ebf462a6f1263c5f149b27a045f69976b
Marcelo-Marinho <marcelohenrique0050@gmail.com>
2023-09-08 16:53:16 -0300

    Update java.java
    idk
    
    Co-Authored-By: G-Delboni <120760974+g-delboni@users.noreply.github.com>

--- java.java
+++ java.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world?");
+    
+    Scanner scan = new Scanner(System.in);
+    String input = scan.nextLine();
+
+    System.out.println(input);
   }
 }
